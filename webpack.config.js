0ce227ef87923ece172826471f689c0f960e19ff
Shi Lei <external.z02lshi@decathlon.com>
2019-12-12 17:20:43 +0800

    finished for class 1

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-const ExtractPlugin =require('extract-text-webpack-plugin')
-
+// const ExtractPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -21,25 +21,15 @@ const config = {
         }, {
             test: /\.jsx$/,
             loader: 'babel-loader'
-        }, {
-            test: /\.styl/,
-            use: [
-                'vue-style-loader',
-                'css-loader',
-                {
-                    loader: 'postcss-loader',
-                    options: { sourceMap: true }
-                },
-                'stylus-loader'
-            ]
         },
+        // {
+        //     test: /\.css$/,
+        //     // loader: 'css-loader'
+        //     use: [
+        //         'style-loader', 'css-loader','postcss-loader'
+        //     ]
+        // }, 
         {
-            test: /\.css$/,
-            // loader: 'css-loader'
-            use: [
-                'style-loader', 'css-loader','postcss-loader'
-            ]
-        }, {
             test: /\.(gif|jpg|jpeg|png|svg)$/,
             use: [
                 {
@@ -58,12 +48,39 @@ const config = {
             }
         }),
         new VueLoaderPlugin(),
-        new HTMLPlugin()
-    ]
+        new HTMLPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+          chunks (chunk) {
+            // exclude `my-excluded-chunk`
+            return chunk.name !== 'my-excluded-chunk';
+          }
+        }
+      }        
 }
 
 
 if (isDev) {
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: { sourceMap: true }
+            },
+            'stylus-loader'
+        ]
+    });
     config.devtool = '#cheap-module-eval-source-map',
         config.devServer = {
             port: '8000',
@@ -78,6 +95,42 @@ if (isDev) {
             new webpack.HotModuleReplacementPlugin(),
             new webpack.NoEmitOnErrorsPlugin()
         )
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor : ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js';
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    // you can specify a publicPath here
+                    // by default it uses publicPath in webpackOptions.output
+                    publicPath: './',
+                    hmr: process.env.NODE_ENV === 'development',
+                }
+            },
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: { sourceMap: true }
+            },
+            'stylus-loader'
+        ]
+    });
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: 'styles.[chunkhash].[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+        
+    );
 }
 
 module.exports = config;
